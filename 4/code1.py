24d26bb87f428bbfcdfb31319d5e040c7a13848e
eris <1252629027@qq.com>
2019-06-14 11:53:56 +0800

    Update code1.py

--- 4/code1.py
+++ 4/code1.py
@@ -1,10 +1,17 @@
+
 dict_f = {}
+
 fileon = open("test.txt","r")
+
 str_file = fileon.read()
+
 for i in str_file:
-  if i in [" ",",",".","?","!"]:
-    continue
-   elif i in dict_f:
-    dict[i] += 1
-   else:
-    dict[i] = 1
+
+    if i in [" ",",",".","?","!"]:
+
+        continue
+
+    elif i not in dict_f:
+        dict_f[i] = 1
+    else:
+        dict_f[i] +=1
