d6cdcfd23f1008ee9d75dd987aae854c45e098a5
eris <1252629027@qq.com>
2019-06-14 17:42:53 +0800

    Update code5.py

--- 4/code5.py
+++ 4/code5.py
@@ -1,4 +1,5 @@
 doublelist = eval(input("输入一个二维数组："))
 for i in range(len(doublelist)):
-	for m in range(i):
-		print(doublelist[i][m],end = "")
+    for m in range(i+1):
+        print(doublelist[i][m],end = " ")
+    print("")
