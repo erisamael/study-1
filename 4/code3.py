bf5b8c7635b80e23ef511e515d79b02f3f0e4933
eris <1252629027@qq.com>
2019-06-15 10:52:52 +0800

    Update code3.py

--- 4/code3.py
+++ 4/code3.py
@@ -1,6 +1,12 @@
 def sort(x):
 	newlist = []
 	for i in range(len(x)):
+		newlist.append(mdef sort(x):
+	newlist = []
+	for i in range(len(x)):
 		newlist.append(max(x))
+		x.remove(max(x))
+		return newlist
+ax(x))
 		x.remove(max(x))
 		return newlist
